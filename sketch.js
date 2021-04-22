44843be9994da1c79df8deae5ed6a8f78f0b646c
Coulson Liang <yizhuo.liang.hello@gmail.com>
2021-04-22 21:57:22 +0800

    Adding connections

--- sketch.js
+++ sketch.js
@@ -12,8 +12,21 @@ function draw() {
   frameRate(5);
   let framePosition = frameCount % data.poses.length;
   background(0);
-  console.info("?");
   console.info(framePosition + " --- " + frameCount);
+
+  translate(width / 4, height / 4);
+
+  stroke(50);
+  for (let i = 0; i < data.poses[framePosition].bodypoints.length; i++) {
+    for (let j = 0; j < data.poses[framePosition].bodypoints.length; j++) {
+      line(
+        data.poses[framePosition].bodypoints[i].x,
+        data.poses[framePosition].bodypoints[i].y,
+        data.poses[framePosition].bodypoints[j].x,
+        data.poses[framePosition].bodypoints[j].y
+      );
+    }
+  }
 
   for (let i = 0; i < data.poses[framePosition].bodypoints.length; i++) {
     fill(240);
