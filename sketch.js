1c2f6d299fcc4cd5426040e419cd2e9b2db36d53
Coulson Liang <yizhuo.liang.hello@gmail.com>
2021-04-22 11:09:56 +0800

    visualize the pose

--- sketch.js
+++ sketch.js
@@ -9,5 +9,25 @@ function setup() {
 }
 
 function draw() {
-  background(200);
+  frameRate(5);
+  background(0);
+  console.info("running");
+
+  // draw the lines
+  stroke(80);
+  for (let i = 0; i < data.bodypoints.length; i++) {
+    for (let j = 0; j < data.bodypoints.length; j++) {
+      line(
+        data.bodypoints[i].x + noise(frameCount) * 5,
+        data.bodypoints[i].y + noise(frameCount + 1) * 5,
+        data.bodypoints[j].x + noise(frameCount + 2) * 5,
+        data.bodypoints[j].y + noise(frameCount + 3) * 5
+      );
+    }
+  }
+
+  for (let i = 0; i < data.bodypoints.length; i++) {
+    fill(220);
+    circle(data.bodypoints[i].x, data.bodypoints[i].y, 15);
+  }
 }
